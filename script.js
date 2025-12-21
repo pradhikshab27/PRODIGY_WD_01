839a321061360a63ae6a02ed2c350ab5a6309f79
pradhikshab27 <pradhikshamonish@gmail.com>
2025-12-21 18:26:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 80) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
